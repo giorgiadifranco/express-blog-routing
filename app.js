b798918e2a3ee3102d59f60106c43dcd0703f5e4
giorgiadifranco <giorgiadf08@gmail.com>
2024-11-04 16:11:52 +0100

    add response con formato json

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const app = express()
+const postsRicette = require('./db/db.js')
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT
@@ -7,3 +8,18 @@ const PORT = process.env.PORT
 app.listen(PORT, (req, res)=>{
     console.log(`Server is running at ${HOST}:${PORT}`);
 })  
+
+//creazione della rotta
+app.get('/',(req, res)=>{
+
+    res.send('Le mie ricette')
+})
+
+// seconda rotta -- response con formato json
+const posts = require ('./db/db.js')
+app.get('/posts', (req, res)=>{
+    res.json({
+        data: posts,
+        counter: posts.length
+    })
+})
